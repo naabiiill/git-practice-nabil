76e7f085ca2ff81744331b7f8d0ec4b4f3fb48af
naabiiill <nabilmahmud.tech@gmail.com>
2026-08-30 02:26:51 +0600

    Add calculator error handling

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
-from utils import add, subtract, multiply
 from datetime import date
+from utils import add, subtract, multiply
 
 print("Name: Nabil Mahmud")
 print("Today's date:", date.today())
 
-print("Addition:", add(10, 5))
-print("Subtraction:", subtract(10, 5))
+try:
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
 
-print("Multiplication:", multiply(10, 5))
+    print("Addition:", add(a, b))
+    print("Subtraction:", subtract(a, b))
+    print("Multiplication:", multiply(a, b))
+
+except ValueError:
+    print("Please enter valid numbers.")
