6b8541a412f7811c2ef4867b7bc15b0561608f2f
naabiiill <nabilmahmud.tech@gmail.com>
2026-08-30 02:23:51 +0600

    Add multiplication function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
+from utils import add, subtract, multiply
 from datetime import date
-from utils import add, subtract
 
 print("Name: Nabil Mahmud")
 print("Today's date:", date.today())
 
 print("Addition:", add(10, 5))
 print("Subtraction:", subtract(10, 5))
+
+print("Multiplication:", multiply(10, 5))
