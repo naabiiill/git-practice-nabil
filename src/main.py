3e4fae1a3a7fdbca7e8dc014acec891caad6b9b9
naabiiill <nabilmahmud.tech@gmail.com>
2026-08-30 02:14:09 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from datetime import date
+from utils import add, subtract
 
 print("Name: Nabil Mahmud")
 print("Today's date:", date.today())
+
+print("Addition:", add(10, 5))
+print("Subtraction:", subtract(10, 5))
